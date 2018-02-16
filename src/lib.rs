36e6decd76d88e2789f9f9241ddafcb0d870865d
James Waples <jamwaffles@gmail.com>
2018-02-16 22:24:31 +0000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![no_std]
+
+extern crate embedded_hal as hal;
+
+use hal::serial;
+
+pub struct ESP8266<Serial> {
+	serial: Serial
+}
+
+impl<Serial> ESP8266<Serial> where
+    Serial: hal::serial::Read<u8> + hal::serial::Write<u8>
+    {
+    	fn new(serial: Serial) -> Self {
+    		Self {
+    			serial: serial,
+    		}
+    	}
 }
