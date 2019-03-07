d7daf0adf112c894f7c34d8907320f3e91730754
Travis CI User <travis@example.org>
2019-03-07 18:50:55 +0000

    Automatic Travis documentation build

---
+++ master/stm32f4/stm32f411/rcc/ahb2lpenr/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["OTGFSLPENR","Possible values of the field `OTGFSLPEN`"],["OTGFSLPENW","Values that can be written to the field `OTGFSLPEN`"]],"struct":[["R","Value read from the register"],["W","Value to write to the register"],["_OTGFSLPENW","Proxy"]]});
---
+++ master/stm32f4/stm32f411/rcc/ahb2rstr/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["OTGFSRSTR","Possible values of the field `OTGFSRST`"],["OTGFSRSTW","Values that can be written to the field `OTGFSRST`"]],"struct":[["R","Value read from the register"],["W","Value to write to the register"],["_OTGFSRSTW","Proxy"]]});
